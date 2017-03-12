42d8c82bfa88bbbafe4a34a1c6f342698e6d1870
codybank789 <codybank789@gmail.com>
2017-03-12 21:24:08 +0700

    Fix client listener

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -53,6 +54,7 @@ public class Client
         ) {
             Listener listener = new Listener(in);
             listener.start();
+            out.println("Hello this is " + InetAddress.getLocalHost().getHostAddress() + " (local IP)");
             String userInput;
             while ((userInput = stdIn.readLine()) != null) {
                 out.println(userInput);
